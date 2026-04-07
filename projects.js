ee9df92ceddc3bcf9c08392c7625fc2449ec18a4
Lea1004 <lea.jazbinsek01@gmail.com>
2026-04-07 22:56:17 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -124,7 +124,7 @@ const projects = {
           "sl": "Ilustrirani dogodkovni plakat"
         },
         "images": [
-          "projects_assets/project5/illustration_title.png",
+          "projects_assets/project5/illustration_big.png",
         ],
         "sections": [
           {
